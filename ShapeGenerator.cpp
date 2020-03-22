1988b96cfa4b27ec1d11004b48a0a2931b41f580
Biehrer <jonas.biehrer@hotmail.de>
2020-03-22 17:44:10 +0100

    Added ShapeGenerator, which offers functions to generate vertices for primitive objects

---
+++ ShapeGenerator.cpp
@@ -0,0 +1,41 @@
+//#include "ShapeGenerator.h"
+/*
+QVector<float> ShapeGenerator::makeTriangleAtPos(float x, float y)
+{
+
+	float x_ = x; /// this->width();
+	float y_ = y; /// this->height();
+
+		QVector<float> vertexes;
+		vertexes.append(x_);
+		vertexes.append(y_);
+		vertexes.append(0.0f);
+
+		//right bottom edge
+		vertexes.append(x_ + 0.2f);
+		vertexes.append(y_ + 0.0f);
+		vertexes.append(0.0f);
+
+		//top of triangle
+		vertexes.append(x_ + 0.0f);
+		vertexes.append(y_ + 0.2f);
+		vertexes.append(0.0f);
+
+		return vertexes;
+}
+
+
+
+QVector<float> ShapeGenerator::makeQuadAtPos(float x, float y)
+{
+
+	float x_ = x; /// this->width();
+	float y_ = y; /// this->height();
+
+	QVector<float> vertexes;
+
+
+
+	return vertexes;
+}
+*/
