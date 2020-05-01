0d498cb0085463b7f423e764126f7e4a9b1e2a18
Biehrer <jonas.biehrer@hotmail.de>
2020-05-01 21:13:16 +0200

    Fixed the RingBuffer implementation and added some operator overloading for Position3D_TC types

--- CircularBuffer.h
+++ CircularBuffer.h
@@ -7,7 +7,13 @@
 #include <vector>
 #include <iostream>
 #include <assert.h>
+#include <vector>
+#include <algorithm>
 #include <mutex>
+#include <atomic>
+
+// forward declaration
+struct Timestamp_TP;
 
 // Interface types of the CircularBuffer
 //! Representation of a position in 3d space containing three components: x, y and z
@@ -31,7 +37,7 @@ public:
     {
     }
 
-    Position3D_TC(Position3D_TC<ElementType_TP>& position)
+    Position3D_TC(const Position3D_TC<ElementType_TP>& position)
         : 
         _x(position._x),
         _y(position._y),
@@ -49,6 +55,7 @@ public:
     //! z value
     ElementType_TP _z;
 
+    // Operator overloads
 public:
     Position3D_TC<ElementType_TP>& operator=(const Position3D_TC<ElementType_TP>& lhs) {
         // self-assignment guard
@@ -61,6 +68,9 @@ public:
         return *this;
     }
 
+    bool operator==(const Position3D_TC<ElementType_TP>& lhs) {
+        return (_x == lhs._x && _y == lhs._y && _z == lhs._z);
+    }
 
     friend std::ostream& operator<<(std::ostream& s, const Position3D_TC& lhs) {
         s << "Element: " << "x: " << lhs._x
@@ -72,21 +82,163 @@ public:
 
 };
 
+
+
 //! Circular buffer class used as input bufer for OGLChart_C
 //! After data is read with ReadLatestData(),
 //! new data inside InsertAtHead(..) is written at the beginning of the buffer
-template<typename ElementType_TP>
-class CircularBuffer_TC
+template<typename T>
+class RingBuffer_TC
 {
     // Construction / Destruction / Copying..
 public:
-    CircularBuffer_TC(int size)
-        : _size(size)
+    RingBuffer_TC(int size)
+        :
+        _max_size(size),
+        _data_series_buffer(size),
+        _number_of_elements(0), 
+        _timestamps(size)
     {
-        _data_series_buffer = new ElementType_TP[_size];
+        _data_series_buffer.reserve(size);
+        _data_series_buffer.resize(size);
+        _timestamps.reserve(size);
+        _timestamps.resize(size);
     }
 
-    ~CircularBuffer_TC()
+    ~RingBuffer_TC() 
+    {
+    }
+
+    // Public access functions
+public:
+    //! Insert a new element inside the buffer
+    //! If the buffer is full, continue writing at the beginning
+    void InsertAtTail(const T& element)
+    {
+        std::unique_lock<std::mutex> lck(_lock);
+        _data_series_buffer[_tail_idx] = element;
+
+        _tail_idx = (_tail_idx + 1) % _max_size;
+        lck.unlock();
+        ++_number_of_elements;
+    }
+
+    // TODO: Return by reference??!
+
+    //! Returns and removes the last added data from the buffer
+    //!
+    //! \returns removes and returns a copy of the last item
+    const T Pop()
+    {
+        T item;
+        if ( !IsBufferEmpty() ) {
+            std::unique_lock<std::mutex> lck(_lock);
+            item = _data_series_buffer[_head_idx];
+            _head_idx = (_head_idx + 1) % _max_size;
+            --_number_of_elements;
+        } else {
+            std::cout << "buffer empty, nothing to pop" << std::endl;
+        }
+        return item;
+    }
+
+    // TODO: Return by reference??!
+    //! Returns and removes the latest data from the buffer
+    //!
+    //! \returns a vector with all data which was added until the buffer was read the last time
+    const std::vector<T> PopLatest() {
+        std::vector<T> latest_data;
+        while ( !IsBufferEmpty() ) {
+            std::unique_lock<std::mutex> lck(_lock);
+            latest_data.push_back(_data_series_buffer[_head_idx]);
+            _head_idx = (_head_idx + 1) % _max_size;
+            --_number_of_elements;
+        }
+        return latest_data;
+    }
+
+
+    //! Returns the last item which was added to the buffer
+    //! Does not remove the item.
+    //!
+    //!\returns copy of the last item added to the buffer
+     T GetLatestItem() {
+       std::unique_lock<std::mutex> lck(_lock);
+       if( _tail_idx == 0 ){
+         return _data_series_buffer[_max_size -1];
+       } else {
+          return _data_series_buffer[(_tail_idx - 1) % _max_size];
+       }
+    }
+
+    bool IsBufferFull() {
+        std::unique_lock<std::mutex> lck(_lock);
+        return _head_idx == (_tail_idx + 1) % _max_size;
+    }
+
+    //! Returns true when there are no elements inside the buffer
+    bool IsBufferEmpty() {
+        std::unique_lock<std::mutex> lck(_lock);
+        return _head_idx == _tail_idx;
+    }
+
+    //! Returns the current number of elements inside the buffer
+    int Size() {
+        return _number_of_elements; 
+    }
+
+    //! Returns the maximum possible elements 
+    int MaxSize() {
+        std::unique_lock<std::mutex> lck(_lock);
+        return _max_size;
+    }
+
+    const std::vector<T>& constData() const {
+        return _data_series_buffer;
+    }
+    // Private attributes
+private:
+    //! The input buffer
+    std::vector<T> _data_series_buffer;
+
+    //! Current write position inside the buffer
+    //! Is incremented each time new data was added via AddData(...)
+    //! Is resetted ( set to zero ) when the data is written into the charts vertex buffer object _chart_vbo
+    int _head_idx = 0;
+
+    //! The idx from which the buffer was read the last time
+    int _tail_idx = 0;
+
+    //! Size of the buffer (maximum number of elements)
+    int _max_size;
+
+    //! current amount of elements inside the buffer
+    std::atomic<unsigned int> _number_of_elements;
+
+    //! Protects shared access to the container
+    std::mutex _lock;
+};
+
+
+
+//! Flush buffer class used as input bufer for OGLChart_C
+//! After data is read with ReadLatestData(),
+//! new data inside InsertAtHead(..) is written at the beginning of the buffer
+//!
+//! A rush buffer:
+//! Once read, data is removed from the buffer!
+template<typename ElementType_TP>
+class InputBuffer_TC
+{
+    // Construction / Destruction / Copying..
+public:
+    InputBuffer_TC(int size)
+        : _max_size(size)
+    {
+        _data_series_buffer = new ElementType_TP[_max_size];
+    }
+
+    ~InputBuffer_TC()
     {
         delete _data_series_buffer;
     }
@@ -96,10 +248,10 @@ public:
 
     void InsertAtHead(ElementType_TP x, ElementType_TP y, ElementType_TP z)
     {
-        assert(_head_idx <= _size && _head_idx >= 0 );
+        assert(_head_idx <= _max_size && _head_idx >= 0 );
         std::unique_lock<std::mutex> lck (_lock);
 
-       if( _head_idx <= _size - 3 ) {
+       if( _head_idx <= _max_size - 3 ) {
             _data_series_buffer[_head_idx] = x;
             _data_series_buffer[_head_idx + 1] = y;
             _data_series_buffer[_head_idx + 2] = z;
@@ -111,7 +263,7 @@ public:
 
     void InsertAtHead(Position3D_TC<ElementType_TP> element)
     {
-        assert(_head_idx <= _size && _head_idx >= 0 );
+        assert(_head_idx <= _max_size && _head_idx >= 0 );
         _data_series_buffer[_head_idx] = element._x;
         _data_series_buffer[_head_idx + 1] = element._y;
         _data_series_buffer[_head_idx + 2] = element._z;
@@ -149,7 +301,7 @@ public:
     // Private attributes
 private:
     //! Size of the buffer (maximum number of elements)
-    int _size;
+    int _max_size;
 
     //! The input buffer - use std vector to use memory locality (cache friendly)
     ElementType_TP* _data_series_buffer;
@@ -166,110 +318,3 @@ private:
     std::mutex _lock;
 };
 
-
-
-//! Circular buffer class used as input bufer for OGLChart_C
-//! After data is read with ReadLatestData(),
-//! new data inside InsertAtHead(..) is written at the beginning of the buffer
-template<typename ElementType_TP>
-class RingBuffer_TC
-{
-    // Construction / Destruction / Copying..
-public:
-    RingBuffer_TC(int size)
-        : _size(size),
-         _data_series_buffer(size)
-    {
-        //_data_series_buffer.reserve(size);
-        _data_series_buffer.resize(size);
-    }
-
-    ~RingBuffer_TC() {
-        //delete _data_series_buffer;
-    }
-
-    // Public access functions
-public:
-
-    void InsertAtTail(ElementType_TP x, ElementType_TP y, ElementType_TP z)
-    {
-        //assert(_head_idx <= _size && _head_idx >= 0);
-        if ( ! IsBufferFull() ) {
-            _tail_idx = (_tail_idx + 1) % _size;
-            //Position3D_TC<ElementType_TP> element(x, y, z);
-            std::unique_lock<std::mutex> lck(_lock);
-            //_data_series_buffer[_tail_idx] = element;
-            _data_series_buffer[_tail_idx] = Position3D_TC<ElementType_TP>(x, y, z);
-            //_data_series_buffer->at(_tail_idx) = Position3D_TC<ElementType_TP>(x, y, z);
-        }
-        else {
-            std::cout << "---BUFFER FULL--- cant insert " << std::endl;
-            // Alternative: start overwriting old values if inserting new items results in an overflow
-        }
-    }
-
-    Position3D_TC<ElementType_TP> Pop() {
-        Position3D_TC<ElementType_TP> item;
-        if ( !IsBufferEmpty() ) {
-            item = _data_series_buffer[_head_idx];
-            _head_idx = (_head_idx + 1 ) % _size;
-        }
-        else {
-            std::cout << "buffer empty, nothing to pop" << std::endl;
-        }
-        return item;
-    }
-
-    std::vector<Position3D_TC<ElementType_TP>> PopLatest() {
-
-        std::vector<Position3D_TC<ElementType_TP>> latest_data;
-
-        if ( !IsBufferEmpty() ) {
-            std::unique_lock<std::mutex> lck(_lock);
-            while ( _head_idx % _size < _tail_idx % _size) {
-                latest_data.emplace_back(_data_series_buffer[_head_idx]);
-                _head_idx = (_head_idx + 1) % _size;
-            }
-        } else {
-            std::cout << "noting to pop, buffer is empty" << std::endl;
-        }
-
-        return latest_data;
-    }
-
-    //! Read latest data from the buffer.
-    //! Returns a vector with zero elements if there is no new data to read (Check this with NewDataToRead())
-    bool NewDataToRead() {
-        std::unique_lock<std::mutex> lck(_lock);
-        return _head_idx < _last_read_idx;
-    }
-
-    bool IsBufferFull() {
-        std::unique_lock<std::mutex> lck(_lock);
-        return _head_idx == (_tail_idx + 1) % _size;
-    }
-
-   bool IsBufferEmpty() {
-       std::unique_lock<std::mutex> lck(_lock);
-       return _head_idx == _tail_idx;
-    }
-
-    // Private attributes
-private:
-    //! Size of the buffer (maximum number of elements)
-    int _size;
-
-    //! The input buffer - use std vector to use memory locality (cache friendly)
-    std::vector<Position3D_TC<ElementType_TP>> _data_series_buffer;
-
-    //! Current write position inside the buffer
-    //! Is incremented each time new data was added via AddData(...)
-    //! Is resetted ( set to zero ) when the data is written into the charts vertex buffer object _chart_vbo
-    int _head_idx = 0;
-
-    //! The idx from which the buffer was read the last time
-    int _tail_idx = 0;
-
-    //! Protects shared access
-    std::mutex _lock;
-};
