f793123e3d32e9d9b76ee5931915cfd6833e6b55
Biehrer <jonas.biehrer@hotmail.de>
2020-05-14 11:50:50 +0200

    Untabify

--- includes/signal_proc_lib/time_signal.h
+++ includes/signal_proc_lib/time_signal.h
@@ -1,1 +1,35 @@
 #pragma once
+
+// STL includes
+#include <iostream>
+
+class FileIO_C {
+
+public:
+
+    FileIO_C();
+    FileIO_C();
+
+public:
+    void Initialize();
+    void Read(std::string filename);
+
+private:
+
+};
+
+template<typename FormatSpec_TP>
+class TimeSignal_C {
+
+public:
+    TimeSignal_C();
+    ~TimeSignal_C();
+
+public:
+
+
+private:
+
+    std::vector<FormatSpec_TP> _data;
+
+};
